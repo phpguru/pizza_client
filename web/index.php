770dbf13a23fbded0449e05254e11c4fd2ac9c84
Geoffrey Hoffman <geoffrey.hoffman@gmail.com>
2016-03-26 11:25:55 -0700

    Added home page route

--- web/index.php
+++ web/index.php
@@ -5,6 +5,10 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app->get('/', function () use ($app) {
+    return 'Pizza Client';
+});
+
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name);
 });
