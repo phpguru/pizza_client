a5a49bde6394e11d65e3343bcb5bc050c0d725f9
Geoff Hoffman <geoffrey.hoffman@gmail.com>
2016-03-27 18:46:16 -0700

    Improvements to adding toppings to pizzas. De-dupe ingredients.

--- app/app.php
+++ app/app.php
@@ -148,13 +148,20 @@ $app->get('/pizza/edit/{id}', function($id) use ($app) {
         
         $fetch_all_toppings = $app['guzzle']->get('/toppings');
         $all_toppings = json_decode($fetch_all_toppings->getBody()->getContents(), true);
-                
+        
+        $de_dupe = [];
         $more_toppings = [];
         foreach ($all_toppings as $possible_topping) {
             if (count($pizza_toppings)) {
                 foreach ($pizza_toppings as $existing_topping) {
                     if ($possible_topping['name'] !== $existing_topping['name']) {
-                        $more_toppings[] = $possible_topping;
+                        if(!in_array($possible_topping, $more_toppings)) {
+                            if (!in_array(trim($possible_topping['name']), $de_dupe)) {
+                                $possible_topping['name'] = ucwords(strtolower($possible_topping['name']));
+                                $more_toppings[] = $possible_topping;
+                                $de_dupe[] = trim($possible_topping['name']);
+                            }
+                        }
                     }
                 }
             } else {
